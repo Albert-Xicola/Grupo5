c061eb0793d8ebde70e0c67aac60c3aa26242980
lluis-muntane <cf2022205@365.stucom.com>
2024-10-09 19:56:42 +0200

    codigo de multiples de 3 y 5

---
+++ Actividad 2/app.py
@@ -0,0 +1,13 @@
+a = 1
+b = 101
+
+while b != a:
+    if a % 3 == 0 :
+        print("FizzBuzz")
+        a = a + 1
+    if a % 5 == 0 :
+        print("FizzBuzz")
+        a = a + 1
+    else:
+        print(a)
+        a = a +1
